6f5c7cc849eae812ea9e6ebddfaee196a1fd1fd2
unknown <t900341@gmail.com>
2022-06-27 15:05:04 +0900

    second commit

--- server/connection.php
+++ server/connection.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$conn = mysqli_connect("localhost","root","","shopping")
+$conn = mysqli_connect("mysql.czhjipeoxdi3.ap-northeast-2.rds.amazonaws.com","root","password","shopping")
         or die("Couldn't connect to database");
 
 
