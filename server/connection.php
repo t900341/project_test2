9ca81b1a4422f0f5dae8aa012098b083b540c437
unknown <t900341@gmail.com>
2022-06-29 14:58:45 +0900

    test 0629

--- server/connection.php
+++ server/connection.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$conn = mysqli_connect("app.czhjipeoxdi3.ap-northeast-2.rds.amazonaws.com","admin","password","shopping")
+$conn = mysqli_connect("mysql.czhjipeoxdi3.ap-northeast-2.rds.amazonaws.com","root","password","shopping")
         or die("Couldn't connect to database");
 
 
